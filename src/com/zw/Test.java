fe9dcd6775cfc8ac5804dab189476d09d7a9d2ed
myworldyoursky <447541224@qq.com>
2017-07-04 13:44:30 +0800

    create project

---
+++ src/com/zw/Test.java
@@ -0,0 +1,10 @@
+package com.zw;
+
+/**
+ * Created by zhangwei on 2017/7/4.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Test Git");
+    }
+}
